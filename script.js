4a4633d21f77ec95f732d12ca5eabd87ca86a878
sang <vuhuysang147@gmail.com>
2023-07-01 10:59:43 +0700

    finish test search

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
 fetch('./data/data.json') // Đường dẫn tới file data.json
   .then(response => response.json())
   .then(data => {
     console.log('data', data);
     const searchInput = document.getElementById('searchInput');
-    const categorySelect = document.getElementById('categorySelect');
+    // const categorySelect = document.getElementById('categorySelect');
     const groupSelect = document.getElementById('groupSelect');
     const timeSelect = document.getElementById('timeSelect');
     const resultsContainer = document.getElementById('resultsContainer');
@@ -13,6 +12,67 @@ fetch('./data/data.json') // Đường dẫn tới file data.json
     console.log('language--languageSelect', languageSelect.value);
 
     let viewsData = [];
+    let selectedOptions = [""];
+
+    let checkCategories1 = document.getElementById('option1');
+    let checkCategories2 = document.getElementById('option2');
+    let checkCategories3 = document.getElementById('option3');
+    let checkCategories4 = document.getElementById('option4');
+    let checkCategories5 = document.getElementById('option5');
+    let checkCategories6 = document.getElementById('option6');
+
+
+
+
+    checkCategories1.addEventListener('click', () => {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      selectedOptions = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+      console.log('Selected Options:', selectedOptions);
+    });
+
+    checkCategories2.addEventListener('click', () => {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      selectedOptions = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+      console.log('Selected Options:', selectedOptions);
+    });
+
+    checkCategories3.addEventListener('click', () => {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      selectedOptions = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+      console.log('Selected Options:', selectedOptions);
+    });
+
+
+    checkCategories4.addEventListener('click', () => {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      selectedOptions = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+      console.log('Selected Options:', selectedOptions);
+    });
+
+    checkCategories5.addEventListener('click', () => {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      selectedOptions = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+      console.log('Selected Options:', selectedOptions);
+    });
+
+    checkCategories6.addEventListener('click', () => {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      selectedOptions = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+      console.log('Selected Options:', selectedOptions);
+    });
+
 
     // ẩn hiển search-box
     const frame_search = document.querySelector(".search-container");
@@ -43,11 +103,28 @@ fetch('./data/data.json') // Đường dẫn tới file data.json
       if (language === 'vi') {
         searchInput.placeholder = 'Tìm kiếm theo tên...';
         // Thay đổi categories
-        const optionElements = categorySelect.getElementsByTagName('option');
-        for (let i = 0; i < optionElements.length; i++) {
-          console.log('option', categorySelect.value)
-          optionElements[i].textContent = data.categories[i].name_vi;
+        const labelElements = document.querySelectorAll('label[for^="option"]');
+        const inputElements = document.querySelectorAll('input[id^="option"]');
+        for (let i = 0; i < labelElements.length; i++) {
+          const labelElement = labelElements[i];
+          const inputElement = inputElements[i];
+
+          const checkboxValue = inputElement.value;
+          if (checkboxValue === 'co-noi-that') {
+            labelElement.textContent = 'Có nội thất';
+          } else if (checkboxValue === 'khong-noi-that') {
+            labelElement.textContent = 'Không nội thất';
+          } else if (checkboxValue === 'trong-nha') {
+            labelElement.textContent = 'Trong nhà';
+          } else if (checkboxValue === 'ngoai-troi') {
+            labelElement.textContent = 'Ngoài trời';
+          } else if (checkboxValue === 'dat-tien') {
+            labelElement.textContent = 'Đắt tiền';
+          } else if (checkboxValue === 're-tien') {
+            labelElement.textContent = 'Rẻ tiền';
+          }
         }
+        // }
         // Thay đổi groups
         const groupOptionElements = groupSelect.getElementsByTagName('option');
         for (let i = 0; i < groupOptionElements.length; i++) {
@@ -64,9 +141,26 @@ fetch('./data/data.json') // Đường dẫn tới file data.json
       } else if (language === 'en') {
         searchInput.placeholder = 'Search by name...';
         // Thay đổi categories
-        const optionElements = categorySelect.getElementsByTagName('option');
-        for (let i = 0; i < optionElements.length; i++) {
-          optionElements[i].textContent = data.categories[i].name_en;
+        const labelElements = document.querySelectorAll('label[for^="option"]');
+        const inputElements = document.querySelectorAll('input[id^="option"]');
+        for (let i = 0; i < labelElements.length; i++) {
+          const labelElement = labelElements[i];
+          const inputElement = inputElements[i];
+
+          const checkboxValue = inputElement.value;
+          if (checkboxValue === 'co-noi-that') {
+            labelElement.textContent = 'Furnished';
+          } else if (checkboxValue === 'khong-noi-that') {
+            labelElement.textContent = 'Unfurnished';
+          } else if (checkboxValue === 'trong-nha') {
+            labelElement.textContent = 'Indoor';
+          } else if (checkboxValue === 'ngoai-troi') {
+            labelElement.textContent = 'Outdoor';
+          } else if (checkboxValue === 'dat-tien') {
+            labelElement.textContent = 'Expensive';
+          } else if (checkboxValue === 're-tien') {
+            labelElement.textContent = 'Affordable';
+          }
         }
         // Thay đổi groups
         const groupOptionElements = groupSelect.getElementsByTagName('option');
@@ -83,21 +177,24 @@ fetch('./data/data.json') // Đường dẫn tới file data.json
     }
 
     changeLanguage(languageSelect.value);
+    console.log('chưa có check ', selectedOptions)
 
     // Tìm kiếm
     function searchViews() {
+      console.log('chưa có check ', selectedOptions)
       const searchTerm = searchInput.value.toLowerCase();
       console.log('tìm kiếm', searchTerm)
       // danh sách phân loại
-      const selectedCategories = Array.from(categorySelect.selectedOptions).map(option => option.value);
-      console.log('lọc các phần tử', typeof selectedCategories, selectedCategories)
-
-      console.log('selectedCategories', selectedCategories)
 
       const filteredViews = data.views
         .filter(view => view[`name_${languageSelect.value}`].toLowerCase().includes(searchTerm))
         .filter(view => Number(view.group_id) === Number(groupSelect.value))
-        .filter(view => selectedCategories.length === 0 || view.categories.some(category => selectedCategories.includes(category)))
+        // .filter(view => selectedOptions.length === 0 || view.categories.some(category => selectedOptions.includes(category)))
+        .filter(view => {
+          console.log('fffff', selectedOptions.length)
+          return view.categories.some(category => selectedOptions.includes(category))
+        })
+
         .filter(view => {
           const h = view.capture_time.split(" ")[1];
           const mh = h.split(":");
@@ -113,6 +210,9 @@ fetch('./data/data.json') // Đường dẫn tới file data.json
             return true; // Không áp dụng lọc theo thời gian
           }
         });
+
+      console.log('ffff--------', filteredViews)
+
       viewsData = filteredViews
       displayResults(viewsData);
     }
@@ -167,8 +267,16 @@ fetch('./data/data.json') // Đường dẫn tới file data.json
     }
 
     searchInput.addEventListener('keyup', searchViews);
-    categorySelect.addEventListener('change', searchViews);
+    // categorySelect.addEventListener('change', searchViews);  
     groupSelect.addEventListener('change', searchViews);
     timeSelect.addEventListener('change', searchViews);
+
+    checkCategories1.addEventListener('change', searchViews);
+    checkCategories2.addEventListener('change', searchViews);
+    checkCategories3.addEventListener('change', searchViews);
+    checkCategories4.addEventListener('change', searchViews);
+    checkCategories5.addEventListener('change', searchViews);
+    checkCategories6.addEventListener('change', searchViews);
+
 
   });
